a16c215eeaee008100e0ef6cd2be048c53ed854a
pegab <p.gabriel@posteo.de>
2025-12-20 11:33:28 +0100

    Initial Commit

---
+++ Project2/Project2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project2App.swift
+//  Project2
+//
+//  Created by Peter Gabriel on 20.12.25.
+//
+
+import SwiftUI
+
+@main
+struct Project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project2Tests/Project2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Project2Tests.swift
+//  Project2Tests
+//
+//  Created by Peter Gabriel on 20.12.25.
+//
+
+import Testing
+@testable import Project2
+
+struct Project2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
